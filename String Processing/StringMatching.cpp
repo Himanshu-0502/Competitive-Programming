8fbef821d763f65ee9c2da5ab3de49d6e7faff4e
Himanshu-0502 <cfhimanshu0502@gmail.com>
2024-08-17 14:01:26 +0530

    Update Matching.cpp

--- String Processing/StringMatching.cpp
+++ String Processing/StringMatching.cpp
@@ -4,9 +4,7 @@ vector<int> prefix(int n,string s){
     vector<int> ans(n);
     for(int i=1;i<n;i++){
         int j=ans[i-1];
-        while(j>0&&s[i]!=s[j]){
-            j=ans[j-1];
-        }
+        while(j>0&&s[i]!=s[j]) j=ans[j-1];
         ans[i]=j+(s[i]==s[j]);
     }
     return ans;
@@ -32,6 +30,22 @@ vector<int> count(int n,vector<int> pref){
         ans[i]++;
     }
     return ans;
+}
+vector<int> Z(string s){
+    int n=s.size();
+    vector<int> z(n);
+    int l=0,r=0;
+    for(int i=1;i<n;i++){
+        if(i<r){
+            z[i]=min(r-i,z[i-l]);
+        }
+        while(i+z[i]<n&&s[z[i]]==s[i+z[i]]) z[i]+=1;
+        if(i+z[i]>r){
+            l=i;
+            r=i+z[i];
+        }
+    }
+    return z;
 }
 int main(){
     ios_base::sync_with_stdio(false);
