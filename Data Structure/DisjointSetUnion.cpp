57664864582873e492b4fda448b2c2e3d9c637a4
Himanshu-0502 <cfhimanshu0502@gmail.com>
2024-11-12 12:14:04 +0530

    DSU update

--- Data Structure/DisjointSetUnion.cpp
+++ Data Structure/DisjointSetUnion.cpp
@@ -26,14 +26,6 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
-    int n,q;
-    cin>>n>>q;
-    DSU dsu(n);
-    while(q--){
-        int t,u,v;
-        cin>>t>>u>>v;
-        if(t==0) dsu.merge(u,v);
-        else cout<<(dsu.find(u)==dsu.find(v))<<"\n";
-    }
+    
     return 0;
 }
