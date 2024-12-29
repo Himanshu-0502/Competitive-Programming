0a0501209cc65eebbcbae095fa48306f42650dab
Himanshu-0502 <cfhimanshu0502@gmail.com>
2024-12-29 22:56:54 +0530

    refactor "SegmentTree.cpp"

--- Data Structure/SegmentTree.cpp
+++ Data Structure/SegmentTree.cpp
@@ -1,63 +1,83 @@
 #include <bits/stdc++.h>
 using namespace std;
-class node{
+
+class Node {
     public:
-    int res;
-    node(int v){
-        res=v;
+    int ans;
+    Node(int val) {
+        ans = val;
     }
 };
-node combine(node a,node b){
-    node c(0);
-    c.res=a.res+b.res;
+
+Node combine(Node a, Node b) {
+    Node c(0);
+    c.ans = a.ans + b.ans;
     return c;
 }
-void build(vector<node> &tree,vector<int> &a,int n){
-    for(int i=0;i<n;i++){
-        tree[i+n]=node(a[i]);
+
+void build(vector<Node> &tree, vector<int> &a, int n) {
+    for (int i = 0; i < n; i++) {
+        tree[i + n] = Node(a[i]);
     }
-    for(int i=n-1;i>0;i--){
-        tree[i]=combine(tree[i<<1],tree[i<<1|1]);
-    }
-}
-node query(vector<node> &tree,int n,int p){
-    node res(0);
-    for(p+=n;p>0;p>>=1){
-        res=combine(res,tree[p]);
-    }
-    return res;
-}
-node query(vector<node> &tree,int n,int l,int r){
-    node resl(0),resr(0);
-    for(l+=n,r+=n;l<r;l>>=1,r>>=1){
-        if(l&1) resl=combine(resl,tree[l++]);
-        if(r&1) resr=combine(tree[--r],resr);
-    }
-    return combine(resl,resr);
-}
-void update(vector<node> &tree,int n,int p,int v){
-    tree[p+=n]=node(v);
-    while(p>>=1){
-        tree[p]=combine(tree[p<<1],tree[p<<1|1]);
+    for (int i = n - 1; i > 0; i--) {
+        tree[i] = combine(tree[(i << 1)], tree[(i << 1 | 1)]);
     }
 }
-void update(vector<node> &tree,int n,int l,int r,int v){
-    for(l+=n,r+=n;l<r;l>>=1,r>>=1){
-        if(l&1) tree[l]=combine(tree[l],node(v)),l++;
-        if(r&1) --r,tree[r]=combine(tree[r],node(v));
+
+Node query(vector<Node> &tree, int n, int l, int r) {
+    Node ansl(0), ansr(0);
+    for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
+        if (l & 1) {
+            ansl = combine(ansl, tree[l]);
+            l += 1;
+        }
+        if (r & 1) {
+            r -= 1;
+            ansr = combine(tree[r], ansr);
+        }
+    }
+    return combine(ansl, ansr);
+}
+
+void update(vector<Node> &tree, int n, int p, int v) {
+    tree[p += n] = Node(v);
+    while (p >>= 1) {
+        tree[p] = combine(tree[(p << 1)], tree[(p << 1 | 1)]);
     }
 }
-void push(vector<node> &tree,vector<int> &a,int n){
-    for(int i=1;i<n;i++){
-        tree[i<<1]=combine(tree[i<<1],tree[i]);
-        tree[i<<1|1]=combine(tree[i<<1|1],tree[i]);
-        tree[i]=node(0);
+
+Node query(vector<Node> &tree, int n, int p) {
+    Node ans(0);
+    for (p += n; p > 0; p >>= 1) {
+        ans = combine(ans, tree[p]);
+    }
+    return ans;
+}
+
+void update(vector<Node> &tree, int n, int l, int r, int v) {
+    for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
+        if (l & 1) {
+            tree[l] = combine(tree[l], Node(v));
+            l += 1;
+        } else {
+            r -= 1;
+            tree[r] = combine(tree[r], Node(v));
+        }
     }
 }
-int main(){
+
+void push(vector<Node> &tree, int n) {
+    for (int i = 1; i < n; i++) {
+        tree[(i << 1)] = combine(tree[(i << 1)], tree[i]);
+        tree[(i << 1 | 1)] = combine(tree[(i << 1 | 1)], tree[i]);
+        tree[i] = Node(0);
+    }
+}
+
+int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
-    
+
     return 0;
 }
