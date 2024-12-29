b75183d448e9bbe8f4d09f7d28ae272eb958b151
Himanshu-0502 <cfhimanshu0502@gmail.com>
2024-12-29 23:19:30 +0530

    refactor DSU

--- Data Structure/DisjointSetUnion.cpp
+++ Data Structure/DisjointSetUnion.cpp
@@ -1,28 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
-class DSU{
+
+class DSU {
     public:
-    vector<int> p,s;
-    DSU(int n){
-        p.assign(n,0);
-        iota(p.begin(),p.end(),0);
-        s.assign(n,1);
+    vector<int> par;
+    vector<int> siz;
+
+    DSU(int n) {
+        par.assign(n, 0);
+        iota(par.begin(), par.end(), 0);
+        siz.assign(n, 1);
     }
-    int find(int v){
-        if(v==p[v]) return v;
-        return p[v]=find(p[v]);
+    
+    int find(int v) {
+        if (v == par[v]) {
+            return v;
+        }
+        return par[v] = find(par[v]);
     }
-    bool merge(int u,int v){
-        if(find(u)==find(v)) return true;
-        u=p[u];
-        v=p[v];
-        if(s[u]<s[v]) swap(u,v);
-        s[u]+=s[v];
-        p[v]=p[u];
+
+    bool same(int u, int v) {
+        return (find(u) == find(v));
+    }
+
+    bool merge(int u, int v) {
+        if (same(u, v)) {
+            return true;
+        }
+
+        u = par[u];
+        v = par[v];
+
+        if (siz[u] < siz[v]) {
+            swap(u, v);
+        }
+
+        siz[u] += siz[v];
+        par[v] = par[u];
+
         return false;
     }
 };
-int main(){
+
+int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
