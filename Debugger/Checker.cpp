7dfebbdea4877e4efca1e5bc081d71c54d4e7c4f
Himanshu-0502 <cfhimanshu0502@gmail.com>
2025-01-15 09:25:41 +0530

    Refactor Checker.cpp and Divisor.cpp

--- Debugger/Checker.cpp
+++ Debugger/Checker.cpp
@@ -1,54 +1,66 @@
 #include <bits/stdc++.h>
 using namespace std;
-long long Number(long long a,long long b){
+
+int Number(int a, int b) {
     mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
-    return (a+rng()%(b-a+1));
+    return (a + rng() % (b - a + 1));
 }
-class Testcase{
-    public:
-    
+
+struct Testcase {
+
 };
-Testcase Generate(){
+
+Testcase generate() {
     Testcase T;
     
     return T;
 }
-int Participant(Testcase T){
-    
-}
-int Judge(Testcase T){
-    
-}
-void Print(Testcase T){
-    
-}
-void Output(int ans){
+
+int participant(Testcase T) {
 
 }
-int main(){
+
+int judge(Testcase T) {
+    
+}
+
+void print(Testcase T) {
+
+}
+
+void output(int ans) {
+    
+}
+
+int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    cout.tie(nullptr);
+
     int tc;
-    cin>>tc;
-    for(int i=1;i<=tc;i++){
-        Testcase T=Generate();
-        int P=Participant(T);
-        int J=Judge(T);
-        if(P==J){
-            cout<<"Passed Testcase: "<<i<<"\n";
-        }
-        else{
-            cout<<"Failed Testcase: "<<i<<"\n";
-            cout<<"Input: ";
-            Print(T);
-            cout<<"Expected: ";
-            Output(J);
-            cout<<"Found: ";
-            Output(P);
-            return 0;
+    cin >> tc;
+
+    for (int i = 1; i <= tc; i++) {
+        Testcase T = generate();
+
+        int P = participant(T);
+        int J = judge(T);
+
+        if (P == J) {
+            cout << "Passed Testcase: " << i << "\n";
+        } else {
+            cout << "Failed Testcase: " << i << "\n";
+
+            cout << "Input" << "\n";
+            print(T);
+
+            cout << "Your Output" << "\n";
+            output(P);
+
+            cout << "Judy Output" << "\n";
+            output(J);
+            exit(0);
         }
     }
-    cout<<"Successfully Passed All Testcases";
+
     return 0;
 }
--- Mathematics/Divisor.cpp
+++ Mathematics/Divisor.cpp
@@ -1,49 +1,56 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector<int> spf(int n){
-    vector<int> factor(n+1);
-    iota(factor.begin(),factor.end(),0);
-    factor[0]=factor[1]=-1;
-    for(int i=2;i*i<=n;i++){
-        if(i!=factor[i]) continue;
-        for(int j=i*i;j<=n;j+=i){
-            if(j!=factor[j]) continue;
-            factor[j]=i;
+
+vector<int> spf(int n) {
+    vector<int> factor(n + 1);
+    iota(factor.begin(), factor.end(), 0);
+    factor[0] = factor[1] = -1;
+
+    for (int i = 2; i * i <= n; i++) {
+        if (i == factor[i]) {
+            for (int j = i * i; j <= n; j += i) {
+                if (j == factor[j]) {
+                    factor[j] = i;
+                }
+            }
         }
     }
     return factor;
 }
-vector<long long> factorize(int n,vector<int> &factor){
-    vector<long long> factors;
-    while(factor[n]!=-1){
+
+vector<int> factorize(int n, vector<int> &factor) {
+    vector<int> factors;
+    while (factor[n] != -1) {
         factors.push_back(factor[n]);
-        n/=factor[n];
+        n /= factor[n];
     }
     return factors;
 }
-vector<long long> divisor(vector<long long> &factors){
-    vector<long long> divisors={1};
-    int n=factors.size();
-    int prev=0;
-    for(int i=0;i<n;i++){
-        if(i!=n-1&&factors[i]==factors[i+1]) continue;
-        int len=divisors.size();
-        long long prime=factors[i];
-        for(int j=0;j+prev<i+1;j++){
-            for(int k=0;k<len;k++){
-                divisors.push_back(divisors[k]*prime);
+
+vector<int> divisor(vector<int> &factors) {
+    vector<int> divisors = {1};
+    int n = factors.size(), prev = 0;
+    
+    for (int i = 0; i < n; i++) {
+        if (i == n - 1 || factors[i] != factors[i + 1]) {
+            int m = divisors.size();
+            long long prime = factors[i];
+            for (int j = 0; j + prev < i + 1; j++) {
+                for (int k = 0; k < m; k++) {
+                    divisors.push_back(divisors[k] * prime);
+                }
+                prime *= factors[i];
             }
-            prime*=factors[i];
+            prev = i + 1;
         }
-        prev=i+1;
     }
-    sort(divisors.begin(),divisors.end());
+    sort(divisors.begin(), divisors.end());
     return divisors;
 }
-int main(){
+
+int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    cout.tie(nullptr);
     
     return 0;
 }
