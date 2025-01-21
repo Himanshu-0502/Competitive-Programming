a85aa1f98398d6c633d422c2bbd270b01980663c
Himanshu-0502 <cfhimanshu0502@gmail.com>
2025-01-21 20:04:20 +0530

    Refactor Combinatorics.cpp

--- Mathematics/Combinatorics.cpp
+++ Mathematics/Combinatorics.cpp
@@ -1,49 +1,65 @@
 #include <bits/stdc++.h>
 using namespace std;
-class Combinatorics{
+
+class Combinatorics {
     public:
-    vector<long long> inv;
-    vector<long long> dfact,ifact;
-    Combinatorics(int n,int mod){
-        dfact.assign(n+1,1);
-        inv.assign(n+1,1);
-        ifact.assign(n+1,1);
-        for(int i=2;i<=n;i++){
-            dfact[i]=(dfact[i-1]*i)%mod;
-            inv[i]=mod-(inv[mod%i]*(mod/i))%mod;
-            ifact[i]=(ifact[i-1]*inv[i])%mod;
+    vector<long long> factorial;
+    vector<long long> inverse;
+    vector<long long> inverseFactorial;
+    
+    Combinatorics(int n, int mod) {
+        factorial.assign(n + 1, 1);
+        inverse.assign(n + 1, 1);
+        inverseFactorial.assign(n + 1, 1);
+
+        for (int i = 2; i <= n; i++) {
+            factorial[i] = (factorial[i - 1] * i) % mod;
+            inverse[i] = mod - (inverse[mod % i] * (mod / i)) % mod;
+            inverseFactorial[i] = (inverseFactorial[i - 1] * inverse[i]) % mod;
         }
     }
-    long long exp(long long a,long long b,int mod){
-        long long ans=1;
-        while(b){
-            if(b&1) ans=((__int128_t)ans*a)%mod;
-            a=((__int128_t)a*a)%mod;
-            b>>=1;
+
+    long long exp(long long a, long long b, long long mod) {
+        long long ans = 1;
+        while (b > 0) {
+            if (b & 1) {
+                ans = (ans * a) % mod;
+            }
+            a = (a * a) % mod;
+            b >>= 1;
         }
         return ans;
     }
-    long long inverse(int n,int mod){
-        return exp(n,mod-2,mod);
+
+    long long inv(long long n, long long mod) {
+        return exp(n, mod - 2, mod);
     }
-    long long nCr(int n,int r,int mod){
-        if(r>n||r<0) return 0;
-        long long ans=dfact[n];
-        ans=(ans*ifact[r])%mod;
-        ans=(ans*ifact[n-r])%mod;
+
+    long long nCr(int n, int r, int mod) {
+        if (r > n || r < 0) {
+            return 0;
+        }
+
+        long long ans = factorial[n];
+        ans = (ans * inverseFactorial[r]) % mod;
+        ans = (ans * inverseFactorial[n - r]) % mod;
         return ans;
     }
-    long long nPr(int n,int r,int mod){
-        if(r>n||r<0) return 0;
-        long long ans=dfact[n];
-        ans=(ans*ifact[n-r])%mod;
+
+    long long nPr(int n, int r, int mod) {
+        if (r > n || r < 0) {
+            return 0;
+        }
+
+        long long ans = factorial[n];
+        ans = (ans * inverseFactorial[n - r]) % mod;
         return ans;
     }
 };
-int main(){
+
+int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    cout.tie(nullptr);
     
     return 0;
 }
